ba1ed268bdff5ab9e53f0ca9bbcd52db46b5c665
arayn123456 <aryanmishra09972@gmail.com>
2024-09-25 21:12:55 +0530

    hello

---
+++ ducat_python.py
@@ -0,0 +1,138 @@
+### Data Types in Python
+          # Primitive Data Types
+                    ## numeric
+                           # int
+                           # float
+                           # complex
+                    ## text
+                           # str
+                    ## binary type
+                           # bool
+                           # bytes
+                    ## Undefined type
+                           # Nonetype
+
+          # Non Primitive Data types or collection types
+                    # list
+                    # tuple
+                    # range
+                    # set
+                    # frozenset
+                    # dict
+                    # bytearray
+
+# a=10   #int
+# b=12.76 #float
+# c=3+7j   # complex
+# c=1j
+
+
+# print(a,type(a))
+# print(b,type(b))
+# print(c,type(c))
+
+# i=654
+# s="654"
+
+# print(s,type(s))
+# print(i,type(i))
+
+# i=input("Enter a number:")  #'8'
+# j=input("Enter other number:")  #'8'
+
+# # print(i,type(i))
+
+# print(int(i)*int(j))
+
+
+# i=False
+# print(i,type(i))
+
+
+
+
+#bytes 
+
+# s1="python"
+
+# s=b"python"   #ASCII codes
+
+# print(type(s))
+
+# print(s1[1])
+# print(s[1])
+
+
+# i=None
+# print(type(i))
+
+##Type Casting(Type Conversion)
+
+# i=0
+
+# print(float(i))
+# print(complex(i))
+
+# print(str(i))   #'12'
+# print(bool(i))
+
+# i=12.86
+
+# print(int(i))
+
+# i=12
+# print(str(i))  #'12'
+
+# s="123"
+
+# print(int(s))
+
+##
+
+l=[1,4.5,"ducat",7+8j,True]
+
+print(type(l))
+
+print(l)
+
+# print(l[0]*5)
+
+l[1]=45
+
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
